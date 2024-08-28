d708b7d00f900d559af1038655d1795125618621
Jared Harrison <jared@jarediscoding.com>
2024-08-28 11:19:47 -0400

    Fix a few react-related issues

--- src/components/CopyrightCard.tsx
+++ src/components/CopyrightCard.tsx
@@ -1,24 +1,22 @@
 import * as React from "react"
-import { Card, Col, Stack } from "react-bootstrap"
+import { Card, Col } from "react-bootstrap"
 import { ImageRequestEmail } from "../util/SiteVariables"
 
 export default () => (
-    <>
-        <Col
-            xs={{span: 12}}
-            md={{span: 12}}
-            lg={{span: 12}}
-            className="flex-center flex-top">
-            <Card>
+    <Col
+        xs={{span: 12}}
+        md={{span: 12}}
+        lg={{span: 12}}
+        key={`item-card-copyright`}>
+        <div className="flex-center flex-top">
+            <Card className="full-width">
                 <Card.Body>
-                    <Card.Text className="flex-center">
-                        <Stack direction="vertical" gap={0}>
-                            <p>All images are copyright to their respective owners.</p>
-                            <p>However, if you would like to claim an image, brand or logo and want it to be removed please <a href={`mailto:${ImageRequestEmail}`}>contact us</a>.</p>
-                        </Stack>
+                    <Card.Text>
+                        All images are copyright to their respective owners.
+                        <br />However, if you would like to claim an image, brand or logo and want it to be removed please <a href={`mailto:${ImageRequestEmail}`}>contact us</a>.
                     </Card.Text>
                 </Card.Body>
             </Card>
-        </Col>
-    </>
+        </div>
+    </Col>
 )
--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -2,56 +2,52 @@ import * as React from "react"
 import { Badge, Card, Col, Stack } from "react-bootstrap"
 
 export default (item: itemData, index: number) => (
-    <>
-        <Col
-            xs={{span: 10, offset: 1}}
-            md={{span: 6, offset: 0}}
-            lg={{span: 4, offset: 0}}
-            className="flex-center flex-top searchableItem"
-            key={`merch-card-item-${item.title}-${index}`}
-            partName={`${item.title}`}
-            partTypes={`${item.typeOfPart.join(",").replaceAll(" ", "")}`}
-            partFabricationMethod={`${item.fabricationMethod.replaceAll(" ", "")}`}>
-                <Card>
-                    {!!item.imageSrc &&
-                        <div className="card-img-holder" style={{backgroundImage: `url('${item.imageSrc}')`}}>
-                            <Card.Img variant="top" src={item.imageSrc} />
-                        </div>
-                    }
+    <Col
+        xs={{span: 10, offset: 1}}
+        md={{span: 6, offset: 0}}
+        lg={{span: 4, offset: 0}}
+        className="flex-center flex-top searchableItem"
+        key={`item-card-${index}`}
+        partname={`${item.title}`}
+        parttypes={`${item.typeOfPart.join(",").replaceAll(" ", "")}`}
+        partfabricationmethod={`${item.fabricationMethod.replaceAll(" ", "")}`}>
+            <Card>
+                {!!item.imageSrc &&
+                    <div className="card-img-holder" style={{backgroundImage: `url('${item.imageSrc}')`}}>
+                        <Card.Img variant="top" src={item.imageSrc} />
+                    </div>
+                }
 
-                    {item.typeOfPart?.length &&
-                        <Stack className="display-over-top" direction="horizontal" gap={1}>
-                            {item.typeOfPart.map((part) => (
-                                <>
-                                    <Badge pill bg="primary">{part}</Badge>
-                                </>
-                            ))}
-                            <Badge pill bg="primary">{item.fabricationMethod}</Badge>
+                {item.typeOfPart?.length &&
+                    <Stack className="display-over-top" direction="horizontal" gap={1}>
+                        {item.typeOfPart.map((part, pillIndex) => (
+                            <Badge key={`item-card-${index}-pill-${pillIndex}`} pill bg="primary">{part}</Badge>
+                        ))}
+                        <Badge pill bg="primary">{item.fabricationMethod}</Badge>
+                    </Stack>
+                }
+                <Card.Body>
+                    <Card.Title as="h3">{item.title}</Card.Title>
+
+                    {item.price &&
+                        <Card.Text>
+                            <i>Price: {item.price ?? "???"}</i>
+                        </Card.Text>
+                    } 
+
+                    {(item.externalUrl || item.dropboxUrl) &&
+                        <Stack direction="vertical" gap={1}>
+                            {item.externalUrl &&
+                                <Card.Link href={item.externalUrl} target="_blank">External Listing</Card.Link>
+                            }
+                            {item.dropboxUrl &&
+                                <Card.Link href={item.dropboxUrl}>ZIP Download{!!item.dropboxZipLastUpdated &&
+                                    <>{` (${item.dropboxZipLastUpdated})`}</>
+                                }</Card.Link>
+                            }
                         </Stack>
                     }
-                    <Card.Body>
-                        <Card.Title as="h3">{item.title}</Card.Title>
-
-                        {item.price &&
-                            <Card.Text>
-                                <i>Price: {item.price ?? "???"}</i>
-                            </Card.Text>
-                        } 
-
-                        {(item.externalUrl || item.dropboxUrl) &&
-                            <Stack direction="vertical" gap={1}>
-                                {item.externalUrl &&
-                                    <Card.Link href={item.externalUrl} target="_blank">External Listing</Card.Link>
-                                }
-                                {item.dropboxUrl &&
-                                    <Card.Link href={item.dropboxUrl}>ZIP Download{!!item.dropboxZipLastUpdated &&
-                                        <>{` (${item.dropboxZipLastUpdated})`}</>
-                                    }</Card.Link>
-                                }
-                            </Stack>
-                        }
-                    </Card.Body>
-                </Card>
-        </Col>
-    </>
+                </Card.Body>
+            </Card>
+    </Col>
 )

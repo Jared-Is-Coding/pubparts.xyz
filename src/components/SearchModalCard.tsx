f40cec09dcca90a268a2b84b77a373ea6d5fec71
Jared Harrison <jared@jarediscoding.com>
2025-02-27 00:19:09 -0500

    Fix global search

--- src/components/SearchModalCard.tsx
+++ src/components/SearchModalCard.tsx
@@ -12,34 +12,41 @@ import React from "react"
  * @param index - a number from a map
  */
 export default (item: ItemData | ResourceData, index: number) => {
-    const linkTo = () => {
+    const getLinks = (d: string) => {
         let url = ""
 
-        if ((item as ItemData).platform?.includes("Floatwheel")) url = "/parts/floatwheel"
-        if ((item as ItemData).platform?.includes("GT/GT-S")) url = "/parts/gt"
-        if ((item as ItemData).platform?.includes("Miscellaneous Items")) url = "/parts/misc"
-        if ((item as ItemData).platform?.includes("Pint/X/S")) url = "/parts/pint"
-        if ((item as ItemData).platform?.includes("VESC Electronics")) url = "/parts/electronics"
-        if ((item as ItemData).platform?.includes("XR")) url = "/parts/xr"
-        if ((item as ResourceData).typeOfResource?.includes("App")) url = "/resources/applications"
-        if ((item as ResourceData).typeOfResource?.includes("Github Repository")) url = "/resources/repositories"
-        if ((item as ResourceData).typeOfResource?.includes("Spreadsheet")) url = "/resources/spreadsheets"
-        if ((item as ResourceData).typeOfResource?.includes("Vendor")) url = "/resources/vendors"
-        if ((item as ResourceData).typeOfResource?.includes("Video Guide")) url = "/resources/videoguides"
-        if ((item as ResourceData).typeOfResource?.includes("Website")) url = "/resources/websites"
-        if ((item as ResourceData).typeOfResource?.includes("Written Guide")) url = "/resources/writtenguides"
+        if (d.includes("Floatwheel")) url = "/parts/floatwheel"
+        else if (d.includes("GT/GT-S")) url = "/parts/gt"
+        else if (d.includes("Miscellaneous Items")) url = "/parts/misc"
+        else if (d.includes("Pint/X/S")) url = "/parts/pint"
+        else if (d.includes("VESC Electronics")) url = "/parts/electronics"
+        else if (d.includes("XR Classic")) url = "/parts/xrclassic"
+        else if (d.includes("XR")) url = "/parts/xr"
+        else if (d.includes("App")) url = "/resources/applications"
+        else if (d.includes("Github Repository")) url = "/resources/repositories"
+        else if (d.includes("Spreadsheet")) url = "/resources/spreadsheets"
+        else if (d.includes("Vendor")) url = "/resources/vendors"
+        else if (d.includes("Video Guide")) url = "/resources/videoguides"
+        else if (d.includes("Website")) url = "/resources/websites"
+        else if (d.includes("Written Guide")) url = "/resources/writtenguides"
 
         return url + `?search=${encodeURI(item.title)}`
     }
 
     return (
-        <Link
-            to={linkTo()}
-            target="_self"
-            className="searchableThing"
-            style={{display: "none"}}
-            key={`thing-card-${index}`}>
-                {(item as ItemData).platform ?? (item as ResourceData).typeOfResource} | {item.title}
-        </Link>
+        <div className="searchableThing" style={{display: "none"}}>
+            {item.title} <>(</>{
+                ((item as ItemData).platform ?? (item as ResourceData).typeOfResource)
+                    .map<React.ReactNode>((i) => (
+                        <a
+                            href={getLinks(i)}
+                            target="_self"
+                            key={`thing-card-${index}-${i}`}>
+                            {i}
+                        </a>
+                    ))
+                    .reduce((p , c) => [p, " | ", c])
+            }<>)</>
+        </div>
     )
 }

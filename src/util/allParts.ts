9121aad5bdaeb1ea76bc383f682d25b0453a9a53
Jared Harrison <jared@jarediscoding.com>
2025-03-07 09:04:16 -0500

    Update parts

--- src/util/allParts.ts
+++ src/util/allParts.ts
@@ -1967,7 +1967,7 @@ const allParts = [
     {
         title: "Ride Foundry: Funwheel Box Cable Covers",
         fabricationMethod: "3d Printed",
-        typeOfPart: ["Controller Box"],
+        typeOfPart: ["Connector Cover"],
         imageSrc: "/images/parts/floatwheel/ridefoundry-cable-cover.png",
         dropboxUrl: "https://www.dropbox.com/scl/fi/5pac5yq3abmc28jzi57wl/ridefoundry-fungi-controller-cable-cover-model_files.zip?rlkey=xid3hmc75tjd0vn38zxbnm82p&st=2uhtl9gx&dl=0",
         dropboxZipLastUpdated: "2025-03-06",

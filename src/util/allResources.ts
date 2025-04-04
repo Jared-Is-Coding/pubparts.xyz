3926ffe19ae5dc5b372e4118adb195345a9ae07e
Jared Harrison <jared@jarediscoding.com>
2025-04-04 10:31:06 -0400

    Update resources

--- src/util/allResources.ts
+++ src/util/allResources.ts
@@ -238,7 +238,7 @@ const allResources = [
     {
         title: "The VESC Bible",
         typeOfResource: ["Website"],
-        externalUrl: "https://thevescbible.notion.site/b59f2fa721fc47549878adbcea43ac5b?v=e0e03c228c4b4c7f88ddb6932ca70db3",
+        externalUrl: "https://pev.dev/c/vesc-bible/11",
         description: "A comprehensive compilation of essential information and responses to FAQ's within the Onewheel VESC community"
     },
     {

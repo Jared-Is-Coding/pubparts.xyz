b04e25632968f66e4f3448441111b95ae57f6a9d
Jared Harrison <jared@jarediscoding.com>
2026-04-19 22:42:08 -0400

    Add .json endpoints to parts and resources pages
    Provides a JSON array of objects containing the unfiltered list data for the page(s)

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,5 +1,24 @@
 import path from "path"
-import type { CreatePagesArgs, CreateWebpackConfigArgs } from "gatsby"
+import { promises as fs } from "fs"
+import type { CreateDevServerArgs, CreatePagesArgs, CreateWebpackConfigArgs } from "gatsby"
+import allParts, {
+    floatwheelParts,
+    gtParts,
+    miscParts,
+    pintParts,
+    vescElectronicsParts,
+    xrClassicParts,
+    xrParts
+} from "./src/util/partsSynced"
+import allResources, {
+    applicationsResources,
+    codeRepositoriesResources,
+    spreadsheetsResources,
+    vendorsResources,
+    videoGuidesResources,
+    websitesResources,
+    writtenGuidesResources
+} from "./src/util/resourcesSynced"
 
 exports.createPages = ({ actions }: CreatePagesArgs) => {
     const { createRedirect } = actions
@@ -34,4 +53,42 @@ exports.onCreateWebpackConfig = ({ actions }: CreateWebpackConfigArgs) => {
             }
         }
     })
+}
+
+const jsonEndpoints: Array<[string, unknown]> = [
+    ["parts.json", allParts],
+    ["parts/electronics.json", vescElectronicsParts],
+    ["parts/floatwheel.json", floatwheelParts],
+    ["parts/gt.json", gtParts],
+    ["parts/misc.json", miscParts],
+    ["parts/pint.json", pintParts],
+    ["parts/xr.json", xrParts],
+    ["parts/xrclassic.json", xrClassicParts],
+    ["resources.json", allResources],
+    ["resources/applications.json", applicationsResources],
+    ["resources/repositories.json", codeRepositoriesResources],
+    ["resources/spreadsheets.json", spreadsheetsResources],
+    ["resources/vendors.json", vendorsResources],
+    ["resources/videoguides.json", videoGuidesResources],
+    ["resources/websites.json", websitesResources],
+    ["resources/writtenguides.json", writtenGuidesResources]
+]
+
+exports.onCreateDevServer = ({ app }: CreateDevServerArgs) => {
+    for (const [endpointPath, data] of jsonEndpoints) {
+        app.get(`/${endpointPath}`, (_req: unknown, res: { type: (contentType: string) => void; send: (body: string) => void }) => {
+            res.type("application/json")
+            res.send(JSON.stringify(data))
+        })
+    }
+}
+
+const writeJsonEndpoint = async (publicPath: string, data: unknown): Promise<void> => {
+    const filePath = path.join(__dirname, "public", publicPath)
+    await fs.mkdir(path.dirname(filePath), { recursive: true })
+    await fs.writeFile(filePath, JSON.stringify(data), "utf8")
+}
+
+exports.onPostBuild = async () => {
+    await Promise.all(jsonEndpoints.map(([publicPath, data]) => writeJsonEndpoint(publicPath, data)))
 }
